407028ba9daac0a5fb94a2aef9f5add1285d1153
Jacob Angel <jacob.angel@gmail.com>
2017-08-23 00:15:15 -0400

    More effin around.

--- src/Bresenham/Example.tsx
+++ src/Bresenham/Example.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 export interface Props {
 }
 
+const RED = [255, 0, 0, 128];
+const BLACK = [0, 0, 0, 128];
 class Canvas extends React.Component<Props> {
   canvas : HTMLCanvasElement | null = null;
 
@@ -16,7 +18,6 @@ class Canvas extends React.Component<Props> {
   }
 
   handleCanvasClick = (e:any) => {
-    console.log(e);
     let { pageX, pageY } = e;
     let { offsetLeft, offsetTop } = e.target;
     const { a, b } = this.state;
@@ -35,20 +36,47 @@ class Canvas extends React.Component<Props> {
     }
   }
 
-  fillPixel = (ctx: CanvasRenderingContext2D, point: Array<number>) => {
+  fillPixel = (ctx: CanvasRenderingContext2D, point: Array<number>, color: Array<number>)=> {
     let imageData = ctx.createImageData(1, 1);
     let data = imageData.data;
-    data[0] = 0;
-    data[1] = 0;
-    data[2] = 0;
-    data[3] = 128;
-    console.log(imageData, point);
+    data[0] = color[0];
+    data[1] = color[1];
+    data[2] = color[2];
+    data[3] = color[3];
     ctx.putImageData(imageData, point[0], point[1]);
+  }
+
+  drawLink(ctx: CanvasRenderingContext2D, a : Array<number>, b: Array<number>) {
+    let [ ax, ay ] = a;
+    let [ bx, by ] = b;
+
+    let dx = Math.abs(bx - ax);
+    let sx = ax < bx ? 1 : -1;
+    let dy = -1 * Math.abs(by - ay);
+    let sy = ay < by ? 1 : -1;
+    let err = dx + dy;
+    let e2;
+
+    while(true) {
+      this.fillPixel(ctx, [ax, ay], BLACK);
+      if (ax === bx && ay === by) {
+        break;
+      }
+      e2 = 2 * err;
+      if (e2 >= dy) {
+        err += dy;
+        ax += sx;
+      }
+      if (e2 <= dx) {
+        err += dx;
+        ay += sy;
+      }
+    }
+
   }
 
   handleDraw() {
     const { a, b } = this.state;
-    console.log('drawring');
     if (!a || !b) {
       return;
     }
@@ -57,11 +85,14 @@ class Canvas extends React.Component<Props> {
       if (ctx === null) {
         return;
       }
+      if (a.length && b.length) {
+        this.drawLink(ctx, a, b);
+      }
       if (a.length) {
-        this.fillPixel(ctx, a);
+        this.fillPixel(ctx, a, RED);
       }
       if (b.length) {
-        this.fillPixel(ctx, b);
+        this.fillPixel(ctx, b, RED);
       }
     }
   }
